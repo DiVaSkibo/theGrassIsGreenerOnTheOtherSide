c7a9e023e952469657838dcb7577f90443421e12
H1kk1k0m0r1 <Danya92005@gmail.com>
2024-08-02 19:50:41 +0200

    character_movement_0.0.1
    +added first movement example
    +jump, left, right, jump control

---
+++ scripts/mc_witch_movement.gd
@@ -0,0 +1,47 @@
+extends CharacterBody2D
+
+
+@export_range(0, 1000) var SPEED: int = 150
+@export_range(-400, 0) var JUMP_HEIGHT: int = -450
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+#@onready var player = $AnimatedSprite2D_b
+@onready var player_animation = $AnimatedSprite2D_b
+
+
+func _physics_process(delta):
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y += gravity * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = JUMP_HEIGHT
+	elif Input.is_action_just_released("jump") and velocity.y < 0:
+		velocity.y = 50
+
+	# Get the input direction and handle the movement/deceleration.
+	# direction | left = -1, no_movement = 0, right = 1 | 
+	var direction = Input.get_axis("left", "right")
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+	
+	# flip_h sprite
+	if direction > 0:
+		player_animation.flip_h = false
+	elif direction < 0:
+		player_animation.flip_h = true
+
+	# play animation
+	if is_on_floor():
+		if direction == 0:
+			player_animation.play("idle")
+		else:
+			player_animation.play("run")
+	else:
+		player_animation.play("jump")
+	
+
+	move_and_slide()
