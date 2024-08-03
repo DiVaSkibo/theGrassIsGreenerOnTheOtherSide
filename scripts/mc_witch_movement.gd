6a8a5007fd0ebebb19f8e9fa7f4f5e67ee7d7354
DiVaSkibo <d.v.skibo@student.khai.edu>
2024-08-03 16:41:35 +0200

    character_movement_0.0.3
    * dash
      + dash on air
      + dash uses ones (after being on the floor)
      + dash duration timer
      * dash cool down timer

--- scripts/mc_witch_movement.gd
+++ scripts/mc_witch_movement.gd
@@ -1,49 +1,40 @@
 extends CharacterBody2D
 
 #		VAR
-@export_range(0, 1000) var SPEED: int = 180
-@export_range(-400, 0) var JUMP_HEIGHT: int = -380
-@export_range(0, 1000) var DASH_SPEED: int = 350
-@export_range(0.0, 2.0) var DASH_DURATION: float = 0.3
+@export_range(0, 1000) var speed := 180
+@export_range(-400, 0) var jump_height := -380
+@export_range(0, 1000) var dash_speed := 350
+@export_range(0.0, 2.0) var dash_duration := 0.3
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-var prevVelocity: Vector2 = Vector2.ZERO
+var prev_velocity := Vector2.ZERO
 
-# Dash variables
-@onready var dash_cool_down = $DashCoolDown
-var can_dash = true
-var is_dashing = false
-var dash_timer = 0.0
-var facing_direction = 1 # 1 - right, -1 - left
 
 #		FUNC
 func _physics_process(delta):
-	# Add the gravity.
-	if not is_on_floor():
-		velocity.y += gravity * delta
-		velocity.y = lerp(prevVelocity.y, velocity.y, 0.8) # air resistence in up_down
-
-	if is_dashing:
-		dash_timer -= delta
-		if dash_timer <= 0.0:
-			end_dash()
+	# is dash activated
+	if not $DashDuration.is_stopped():
+		if $AnimatedSprite2D.flip_h:
+			velocity.x = -dash_speed
+		else:
+			velocity.x = dash_speed
 	else:
+		# Add the gravity.
+		if not is_on_floor():
+			velocity.y += gravity * delta
+			velocity.y = lerp(prev_velocity.y, velocity.y, 0.8) # air resistence in up_down
+
 		# Handle jump.
 		if Input.is_action_just_pressed("jump") and is_on_floor():
-			velocity.y = JUMP_HEIGHT
+			velocity.y = jump_height
 		elif Input.is_action_just_released("jump") and velocity.y < 0:
 			velocity.y *= 0.2
 
 		# Get the input direction and handle the movement/deceleration.
 		var direction = Input.get_axis("left", "right")
 		if direction:
-			velocity.x = direction * SPEED
-			facing_direction = direction # direction for dash
+			velocity.x = direction * speed
 		else:
-			velocity.x = move_toward(velocity.x, 0, SPEED)
-
-		# Handle dash
-		if Input.is_action_just_pressed("dash") and can_dash and is_on_floor():
-			start_dash(facing_direction)
+			velocity.x = move_toward(velocity.x, 0, speed)
 
 		# flip_h sprite
 		if direction > 0:
@@ -51,37 +42,33 @@ func _physics_process(delta):
 		elif direction < 0:
 			$AnimatedSprite2D.flip_h = true
 
-		# play animation
-		if is_on_floor() and not is_dashing:
+		# Play animation
+		if Input.is_action_just_pressed("dash") and $DashDuration.is_stopped() and $DashCoolDown.is_stopped():
+			# start dash
+			velocity.x = dash_speed * direction
+			velocity.y = 0
+			$DashDuration.start()
+			$AnimatedSprite2D.play("dash")
+		elif is_on_floor():
 			if direction == 0:
 				$AnimatedSprite2D.play("idle")
 			else:
 				$AnimatedSprite2D.play("run")
-		elif not is_on_floor() and not is_dashing:
-			velocity.x = lerp(prevVelocity.x, velocity.x, 0.1) # air resistence in left_right
+		else:
+			velocity.x = lerp(prev_velocity.x, velocity.x, 0.1) # air resistence in left_right
 			$AnimatedSprite2D.play("jump")
 
-	prevVelocity = velocity
+	prev_velocity = velocity
 	move_and_slide()
 
-# Start the dash
-func start_dash(direction):
-	is_dashing = true
-	dash_timer = DASH_DURATION
-	can_dash = false
-	velocity.x = DASH_SPEED * direction
-
-	# Start dash animation only once
-	if $AnimatedSprite2D.animation != "dash":
-		$AnimatedSprite2D.play("dash")
-
-	dash_cool_down.start(0.6) # Adjust cooldown time
-
-# End the dash
-func end_dash():
-	is_dashing = false
-	velocity.x = 0
 
 #		SIGNAL
+# dash end
+func _on_dash_duration_timeout():
+	velocity.x = 0
+	$DashCoolDown.start()
+# dash cool down end if mc is on the floor
 func _on_dash_cool_down_timeout():
-	can_dash = true
+	if not is_on_floor():
+		$DashCoolDown.start()
+
