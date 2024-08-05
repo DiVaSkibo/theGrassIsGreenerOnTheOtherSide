55f567b88ef0347d331d0dffa892236325f4ab3c
DiVaSkibo <d.v.skibo@student.khai.edu>
2024-08-05 13:26:12 +0200

    textbox scene 0.0.1
    * textbox script
    * transition scene and script

--- scripts/___transition.gd
+++ scripts/___transition.gd
@@ -1,6 +1,6 @@
 extends CanvasLayer
 
-signal on_transition_finished
+signal finished
 
 
 func play():
@@ -12,7 +12,7 @@ func _on_animation_player_animation_finished(anim_name):
 	match anim_name:
 		"appear":
 			$AnimationPlayer.play("disappear")
-			on_transition_finished.emit()
+			finished.emit()
 		"disappear":
 			$ColorRect.visible = false
 
